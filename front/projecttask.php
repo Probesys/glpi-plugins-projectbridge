bc970fed1d31164808ba9d0ab2329e910686e0f0
Pierre de Vésian <pierre.devesian@probesys.com>
2018-07-25 16:30:40 +0200

    Add project task list -- fix target url

--- front/projecttask.php
+++ front/projecttask.php
@@ -5,6 +5,12 @@ include ('../../../inc/includes.php');
 Session::checkLoginUser();
 
 Html::header('Tâches de projet', $_SERVER['PHP_SELF'], 'tools', 'projecttask');
+
+// force GLPI to point to this page
+global $CFG_GLPI;
+$list_url = rtrim($CFG_GLPI['root_doc'], '/') . '/plugins/projectbridge/front/projecttask.php';
+$_GET['target'] = $list_url;
+
 Search::show('projecttask');
 
 Html::footer();
