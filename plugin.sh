94c6bb25b625a0514c6a9f61be499ebc9f51196d
Philippe GODOT <philippe.godot@probesys.com>
2023-04-27 14:26:55 +0200

    mise a jour plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -84,6 +84,11 @@ sed \
     -e "s/{UNAME}/$UNAME/" \
     -e "s/{VERSION}/$VERSION/" \
     -e "s/{YEAR}/$YEAR/" \
-    -i setup.php hook.php $LNAME.xml README.md
+    -i setup.php hook.php $LNAME.xml
+
+#generate archive and remove plugin folder after
+cd .. && 
+tar -cvjf $LNAME-$VERSION.tbz $LNAME &&
+rm -rf $LNAME
 
 popd > /dev/null
